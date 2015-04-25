2af088af12487c41d8f36824ea36fa7ade30d985
Jeanine Huang <jeanineh@andrew.cmu.edu>
2015-04-25 07:02:50 -0700

    added missing user authentication methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,17 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound do |exception|
     redirect_to home_path, error: "Record not found in the system."
   end
+
+  private
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+  def logged_in?
+    current_user
+  end
+  helper_method :logged_in?
+  
   
 
 end
